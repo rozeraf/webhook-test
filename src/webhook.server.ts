bc9c4a954a2270d705202ad249110e094f165653
Раф <rafabduloff@gmail.com>
2025-08-31 03:42:12 +0500

    добавил импорт config dotenv

--- src/webhook.server.ts
+++ src/webhook.server.ts
@@ -1,5 +1,6 @@
 // src/webhook.server.ts
 import { Bot, webhookCallback } from "grammy";
+import 'dotenv/config';
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import { logger } from "hono/logger";
